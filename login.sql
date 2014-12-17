9a6b3ec0f8aacbad65bbe56d796d8e794630523a
ShuRui Liu <jackliusr@gmail.com>
2014-12-17 10:45:03 +0800

    Create login.sql
    sqlplus environment setting, put in home directory

---
+++ login.sql
@@ -0,0 +1,17 @@
+set serveroutput on size 1000000
+set trimspool on
+set long 5000
+SET LONGCHUNKSIZE  1000
+set linesize 200
+set pagesize 50000
+column plan_plus_exp format a80
+column global_name new_value gname
+set termout off
+SET SQLPROMPT "_USER'@'_CONNECT_IDENTIFIER> "
+set termout on
+
+SET DESCRIBE DEPTH 2
+SET DESCRIBE INDENT ON
+SET DESCRIBE LINE OFF
+
+alter session set NLS_DATE_FORMAT ='yyyy-MM-DD HH24:MI:SS';
